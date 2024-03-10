9019e62521b7c2566646bc966e368f7ad717ac13
Vlad Vasiliev <v14d06@gmail.com>
2024-03-10 14:18:06 +0200

    added changes in tabs

--- app/src/main/java/com/example/drivetracker/ui/order/OrderVehicleScreen.kt
+++ app/src/main/java/com/example/drivetracker/ui/order/OrderVehicleScreen.kt
@@ -2,34 +2,21 @@
 
 package com.example.drivetracker.ui.order
 
-import android.annotation.SuppressLint
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import android.util.Log
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.sharp.AccountCircle
-import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.Card
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Tab
-import androidx.compose.material3.TabRow
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -37,74 +24,63 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.drivetracker.data.entity.Car
+import com.example.drivetracker.data.entity.Truck
 import com.example.drivetracker.model.OrderVehicleUiState
 import com.example.drivetracker.ui.RentWheelsScreen
-import com.example.drivetracker.ui.adding.AddCarScreen
-import com.example.drivetracker.ui.auth.LogInScreen
-import com.example.drivetracker.ui.auth.SignInScreen
-import com.google.firebase.Firebase
-import com.google.firebase.auth.auth
 
 @Composable
 fun OrderVehicleScreen(
     navHostController: NavHostController,
-    viewModel: OrderVehicleViewModel = OrderVehicleViewModel()
-){
+    viewModel: OrderVehicleViewModel = remember { OrderVehicleViewModel() }
+) {
+    val uiState by viewModel.uiState.collectAsState()
     Surface(modifier = Modifier.fillMaxSize()) {
-        val car = Car(0,"AUDI", "Q7", 2019, false, 4)
-        val list= mutableListOf(car)
-
-        for(i in 1..20){
-            list.add(car)
-        }
         Column(Modifier.fillMaxSize()) {
             TopVehicleBar(viewModel)
-            Column {
-                LazyVerticalGrid(
-                    columns = GridCells.Adaptive(minSize = 100.dp)) {
-                    items(list){item ->
-                        DisplayCar(item)
+            LazyVerticalGrid(
+                columns = GridCells.Adaptive(minSize = 100.dp)
+            ) {
+                if (uiState.isTruck) {
+                    items(viewModel.getTrucks()) { truck ->
+                        DisplayTruck(truck = truck)
+                    }
+                } else {
+                    items(viewModel.getCars()) { car ->
+                        DisplayCar(car = car)
                     }
                 }
             }
-
         }
         BottomAppBarWithThreeSections(navHostController)
-
     }
 }
 
 @Composable
 fun TopVehicleBar(
     viewModel: OrderVehicleViewModel
-){
-    Column(verticalArrangement = Arrangement.Top) {
-        val tabs = listOf("Cars","Trucks")
-        var selectedTabIndex by remember {
-            mutableIntStateOf(0)
-        }
-        TabRow(
-            selectedTabIndex = selectedTabIndex
-        ) {
-            tabs.forEachIndexed { index, text ->
-                Tab(
-                    selected = selectedTabIndex == index,
-                    onClick = {
-                        selectedTabIndex = index
-                        viewModel.changeVehicle(selectedTabIndex)
-                              },
-                    text = { Text(text) }
-                )
-            }
+) {
+    val tabs = listOf("Cars", "Trucks")
+    var selectedTabIndex by remember {
+        mutableIntStateOf(0)
+    }
+    TabRow(
+        selectedTabIndex = selectedTabIndex
+    ) {
+        tabs.forEachIndexed { index, text ->
+            Tab(
+                selected = selectedTabIndex == index,
+                onClick = {
+                    selectedTabIndex = index
+                    viewModel.changeVehicle(selectedTabIndex)
+                    Log.e("M", "Turn $selectedTabIndex")
+                },
+                text = { Text(text) }
+            )
         }
     }
 }
-
 
 @Composable
 fun BottomAppBarWithThreeSections(
@@ -122,31 +98,40 @@ fun BottomAppBarWithThreeSections(
                 horizontalArrangement = Arrangement.SpaceBetween,
                 verticalAlignment = Alignment.Bottom
             ) {
-                IconButton(onClick = { /* Handle action */ }) {
+                IconButton(onClick = { /* Handle home action */ }) {
                     Icon(imageVector = Icons.Default.Home, contentDescription = "Home")
                 }
                 IconButton(onClick = { navHostController.navigate(RentWheelsScreen.AddCar.name) }) {
-                    Icon(imageVector = Icons.Default.Add, contentDescription = "Home")
-
+                    Icon(imageVector = Icons.Default.Add, contentDescription = "Add")
                 }
-                IconButton(onClick = { /* Handle action */ }) {
-                    Icon(imageVector = Icons.Sharp.AccountCircle, contentDescription = "Home")
+                IconButton(onClick = { /* Handle account action */ }) {
+                    Icon(imageVector = Icons.Sharp.AccountCircle, contentDescription = "Account")
                 }
             }
         }
     }
-
 }
 
 @Composable
-fun DisplayCar(car:Car){
+fun DisplayCar(car: Car) {
     Card(modifier = Modifier.padding(15.dp)) {
         Column(Modifier.padding(10.dp)) {
             Text(text = car.brand)
             Text(text = car.model)
             Text(text = car.year.toString())
             Text(text = car.numberSeats.toString())
+        }
+    }
+}
 
+@Composable
+fun DisplayTruck(truck: Truck) {
+    Card(modifier = Modifier.padding(15.dp)) {
+        Column(Modifier.padding(10.dp)) {
+            Text(text = truck.brand)
+            Text(text = truck.model)
+            Text(text = truck.year.toString())
+            Text(text = truck.cargoCapacity.toString())
         }
     }
 }
--- app/src/main/java/com/example/drivetracker/ui/order/OrderVehicleViewModel.kt
+++ app/src/main/java/com/example/drivetracker/ui/order/OrderVehicleViewModel.kt
@@ -1,6 +1,8 @@
 package com.example.drivetracker.ui.order
 
 import androidx.lifecycle.ViewModel
+import com.example.drivetracker.data.entity.Car
+import com.example.drivetracker.data.entity.Truck
 import com.example.drivetracker.model.OrderVehicleUiState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -15,5 +17,25 @@ class OrderVehicleViewModel:ViewModel() {
         _uiState.update { currentState ->
             currentState.copy(isTruck = index!=0)
         }
+    }
+
+    fun getCars(): MutableList<Car> {
+        val car = Car(0,"AUDI", "Q7", 2019, false, 4)
+        val list= mutableListOf(car)
+
+        for(i in 1..20){
+            list.add(car)
+        }
+        return list
+    }
+
+    fun getTrucks(): MutableList<Truck>{
+        val trucks = Truck(0,"Bugr", "Q7", 2019, false, 182364.3)
+        val list= mutableListOf(trucks)
+
+        for(i in 1..20){
+            list.add(trucks)
+        }
+        return list
     }
 }
